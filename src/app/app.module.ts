1b498710820c48bfe9cde23396a04151d9220489
Gutsover <mylenevandaele@gmail.com>
2020-02-10 21:22:45 +0100

    add CardComponent & Card interface

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './home/components/home/home.component';
+import { CardComponent } from './home/components/card/card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/card.ts
@@ -0,0 +1,7 @@
+export interface Card {
+    tabindex: number;
+    image: string;
+    imageDescription: string;
+    title: string;
+    text: string;
+}
